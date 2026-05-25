e842157967bceda3c53b41a2dfe92ea07c63a1c2
Marius Gedminas <marius@gedmin.as>
2026-05-25 11:04:59 +0300

    Correctly detect the end of a scope

--- pythonx/taghelper_yaml.py
+++ pythonx/taghelper_yaml.py
@@ -18,17 +18,20 @@ class Scope(typing.NamedTuple):
 
 def parse(buffer, tags):
     key_rx = re.compile(r'^(\s*)([-_a-zA-Z][-_a-zA-Z0-9]*):\s*(?:#.*)?$')
+    other_rx = re.compile(r'^(\s*)[^#]')
     stack = []
     for n, line in enumerate(buffer, 1):
-        m = key_rx.fullmatch(line)
-        if m is None:
-            continue
-        indent = len(m[1].expandtabs())
-        name = m[2]
-
-        while stack and stack[-1].indent >= indent:
-            stack[-1].tag.lastlineno = n - 1
-            stack.pop()
-        parent = stack[-1].qualname if stack else ''
-        tag = tags.add(parent + name, n)
-        stack.append(Scope(indent, parent + name + '.', tag))
+        if m := key_rx.fullmatch(line):
+            indent = len(m[1].expandtabs())
+            name = m[2]
+            while stack and stack[-1].indent >= indent:
+                stack[-1].tag.close(n - 1)
+                stack.pop()
+            parent = stack[-1].qualname if stack else ''
+            tag = tags.add(parent + name, n)
+            stack.append(Scope(indent, parent + name + '.', tag))
+        elif m := other_rx.match(line):
+            indent = len(m[1].expandtabs())
+            while stack and stack[-1].indent >= indent:
+                stack[-1].tag.close(n - 1)
+                stack.pop()
