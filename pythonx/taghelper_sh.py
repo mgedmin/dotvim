2c3e3fedb2828a380925c87736a33fd7986d2fe4
Marius Gedminas <marius@gedmin.as>
2026-05-28 15:46:29 +0300

    Add parser for shell functions

---
+++ pythonx/taghelper_sh.py
@@ -0,0 +1,18 @@
+import re
+
+
+TAGHELPER_PLUGIN_API_VERSION = 1
+TAGHELPER_SYNTAX = 'bash'
+
+
+FUNCTION_RX = re.compile(r'^(?:function\s+)?(\w+)\s*[(][)]\s*[{]')
+END_RX = re.compile('^}')
+
+
+def parse(buffer, tags):
+    for n, line in enumerate(buffer, 1):
+        if m := FUNCTION_RX.match(line):
+            name = m[1]
+            tags.add(name, n, autoclose=True)
+        elif m := END_RX.match(line):
+            tags.autoclose(n)
